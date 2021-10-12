6cc0d6e805d293052d6231f6955a31911c305cb9
Anna Novak <anovak@Annas-MacBook-Air.local>
2021-10-12 12:26:12 -0500

    September 2021 updates to by_county.html and by_region.html files, add versions of each map excluding law graduates, and add functions in Attys_Offices_Counties_test.R to automate creation of the .html files. A few minor edits to poverty-estimates-to-spreadsheet.R

--- Scripts/Attys_Offices_Counties_test.R
+++ Scripts/Attys_Offices_Counties_test.R
@@ -1,4 +1,5 @@
-## Modified version of Attys_Offices_Counties.R that excludes law grads
+## Adapting Attys_Offices_Counties.R to produce versions of the by_region adn by_county maps 
+## with and without law graduates. 
 
 library(readr)
 library(readxl)
@@ -7,52 +8,59 @@ library(tigris)
 library(sf)
 library(leaflet)
 library(htmltools)
-library(tidycensus)
+library(tidycensus) #install dev version 1.1 with: remotes::install_github("walkerke/tidycensus")
+library(htmlwidgets)
 
-#Read in data that pairs each office with a county
-office_counties <- read_excel("./Data/TRLA_Counties_by_Office.xlsx") %>%
-  mutate(Office = str_to_upper(Office))
+# Load office - county pairings
+office_county_matches <- read_excel("./Data/TRLA_Counties_by_Office.xlsx") %>%
+  clean_names() %>%
+  mutate(office = str_to_upper(office))
 
-#Not sure what's up with the Sinton office, so just converting that to Corpus
-office_counties$Office[office_counties$Office=="SINTON"] <- "CORPUS CHRISTI"
+# Convert Sinton to Corpus Christi
+office_county_matches$office[office_county_matches$office=="SINTON"] <- "CORPUS CHRISTI"
 
-#Read in a list of every county in our service area that includes GEOIDs
-service_area <- read.csv("./Data/SACount.csv") %>%
-  rename(GEOID = 1)
-
-#List of attorneys , removing irrelevant offices
-# attys_civil_old <- read.csv("./Data/Attorneys_6.1.csv") %>%
-#   filter(!str_detect(Office, "PD") & !str_detect(Office, "NASHVILLE") & !str_detect(Office, "REMOTE")
-#          & !str_detect(Office, "DALLAS"))
+# Load list of counties in TRLA service area with GEOIDS
+trla_counties <- read.csv("./Data/SACount.csv") %>% clean_names()
 
 
-#Updating the above cleaning steps for September staff list:
-attys_civil <- read.csv("./Data/2021_09_atty_list.csv") %>% filter(!str_detect(Office, "REMOTE") & !str_detect(Office, "NASHVILLE")
-                                                                   & !str_detect(Office, "DALLAS") & !str_detect(Office, "TAJ"))
+# Load latest civil attorney staff list
+attys_list <- read.csv("./Data/September 2021 ATTY List.csv") %>% 
+  clean_names() %>%
+  
+  # Filter out offices outside of the 68 county service area
+  filter(
+    !str_detect(office, "REMOTE") &  !str_detect(office, "NASHVILLE") & 
+    !str_detect(office, "DALLAS") & !str_detect(office, "TAJ")
+    ) %>% 
+  
+  # Clean up office names
+  mutate(
+    office = case_when(
+                (office == "CORPUS COURTHOUSE" | office == "CORPUS PUEBLO") ~ "CORPUS CHRISTI",
+                (office == "MERCEDES" | office == "HARLINGEN") ~ "BROWNSVILLE",
+                 office == "EL PASO/ NY" ~ "EL PASO",
+                 office == "VICTORIA LAW CTR (CY" ~ "VICTORIA",
+                 office == "DILLEY" ~ "SAN ANTONIO",
+                 TRUE ~ office)
+    )
 
 
-#Additional text changes so that atty list matches county list
-attys_civil$Office[attys_civil$Office == "CORPUS COURTHOUSE" | attys_civil$Office == "CORPUS PUEBLO"] <- "CORPUS CHRISTI"
-attys_civil$Office[attys_civil$Office == "MERCEDES" | attys_civil$Office == "HARLINGEN"] <- "BROWNSVILLE"
-attys_civil$Office[attys_civil$Office == "EL PASO/ NY"] <- "EL PASO"   
-attys_civil$Office[attys_civil$Office == "VICTORIA LAW CTR (CY"] <- "VICTORIA" 
-attys_civil$Office[attys_civil$Office == "DILLEY"] <- "San Antonio" 
+# Subset to civil attorneys who handle cases (exclude law grads) 
+attys_list_nlg <- attys_list %>% filter(job_title != "LAW GRADUATE")
 
-# Excluding law grads from the calculations
-attys_civil_nlg <- attys_civil %>% filter()
 
+## ----------- Load ACS data
 
 # Register for your unique API key here: https://api.census.gov/data/key_signup.html
-# Save your key somewhere outside of this project directory, so it won't end up accidentally tracked by Git
-
+# Save your key outside of this project directory
 key <- read.delim("../credentials/census_api_key.txt", header = FALSE)
 
-# Add your Census API key to your environment
+# Add Census API key to your environment
 census_api_key(key)
 
 
-#read in Census data
-poverty_data <- get_acs(geography = "county",
+# Load ACS data
+poverty_data_raw <- get_acs(geography = "county",
                         table = "C17002",
                         survey = "acs5",
                         year = 2019,
@@ -62,7 +70,8 @@ poverty_data <- get_acs(geography = "county",
                         geometry = FALSE
 )
 
-poverty_data <- subset(poverty_data, GEOID %in% service_area$GEOID)
+
+poverty_data <- subset(poverty_data_raw, GEOID %in% trla_counties$geoid)
 
 
 poverty_data <- poverty_data %>%
@@ -71,98 +80,178 @@ poverty_data <- poverty_data %>%
   group_by(GEOID) %>%
   mutate(Under200 = sum(estimate),
          Under200MOE = moe_sum(moe,estimate),
-         Perc_MOE = Under200MOE/Under200) %>%
+         Perc_MOE = round(Under200MOE/Under200 * 100, 2)) %>%
   filter(variable == "C17002_002") %>%
   select(-estimate, -moe) %>%
   mutate(NAME = str_replace(NAME, " County, Texas", ""))
 
 
-#Calculate poverty population per office area
 
-#Generate count of attorneys by office; join with the office/county matching data
-attys_civil_cty <- attys_civil %>%
-  group_by(Office) %>%
-  summarise(attys = n()) %>%
-  full_join(office_counties) %>%
-  group_by(Office) %>%
+
+# ----- Functions to build the spatial data layers
+
+get_estimates <- function(poverty_data, attys, trla_counties) {
   
-  #Then generate number of attorneys in a region divided by number of counties in the region
-  mutate(attys_per_cty = attys/n()) %>%
+  #Calculate poverty population per office area
   
-  #Join with poverty data; create a measure for attorneys per 10,000 people under 200% FPL
-  left_join(poverty_data, by = c("County" = "NAME")) %>%
-  group_by(Office) %>%
-  mutate(ppl_per_atty_region = round(Under200/attys,2)) %>%
-  mutate(Poverty_pop_region = sum(Under200))
+  # Get a count of attorneys by office and join the office-county matching data
+  attys_county <- attys %>%
+    group_by(office) %>%
+    summarise(attys = n()) %>%
+    full_join(office_county_matches) %>%
+    group_by(office) %>%
+    
+    # Number of attorneys in a region divided by number of counties in the region
+    mutate(attys_per_county = attys/n()) %>%
+    
+    # Join poverty data; create a measure for population under 200% FPL per attorney
+    left_join(poverty_data, by = c("county" = "NAME")) %>%
+    group_by(office) %>%
+    mutate(pop_per_atty_region = round(Under200/attys,2)) %>%
+    mutate(poverty_pop_region = sum(Under200))
+  
+  
+  # Create a df of this data per office
+  attys_office <- attys_county %>%
+    distinct(office, .keep_all = TRUE) %>%
+    mutate(pop_per_atty_region = round(poverty_pop_region/attys,2))
+  
+  
+  # Create spatial layer of COUNTY polygons with attorney and poverty data
+  county_estimates <- counties(state = "TX", cb = FALSE) %>%
+    subset(GEOID %in% trla_counties$geoid) %>%
+    geo_join(attys_county, by_df = "county", by_sp = "NAME") %>%
+    mutate(pop_per_atty_county = round(Under200/attys,2))
+  
+  
+  # Create spatial layer of REGION polygons with attorney and poverty data
+  region_estimates <- county_estimates %>%
+    group_by(office) %>%
+    summarise(geometry = st_union(geometry)) %>%
+    ungroup() %>%
+    geo_join(attys_office, by_df = "office", by_sp = "office")
+  
+  
+  estimates <- list(county_estimates, region_estimates)
+  
+  return(estimates)
+  
+}
 
 
 
-#Create a list of this data specifically per office
-attys_civil_office <- attys_civil_cty %>%
-  distinct(Office, .keep_all = TRUE) %>%
-  mutate(ppl_per_atty_region = round(Poverty_pop_region/attys,2))
+# ------ Build county map function
 
-TRLA_counties <- counties(state = "TX", cb = FALSE) %>%
-  subset(GEOID %in% service_area$GEOID) %>%
-  geo_join(attys_civil_cty, by_df = "County", by_sp = "NAME") %>%
-  mutate(ppl_per_atty_cty = round(Under200/attys,2))
+build_maps <- function(county_estimates, region_estimates) {
 
-TRLA_regions <- TRLA_counties %>%
-  group_by(Office) %>%
-  summarise(geometry = st_union(geometry)) %>%
-  ungroup() %>%
-  geo_join(attys_civil_office, by_df = "Office", by_sp = "Office")
+    # 1) Build county map
+    # Why hardcode the county bins but not region bins?
+    bins <- c(0, 100, 200, 500, 1000, 10000, 15000, 20000, Inf)
+    
+    palette_counties <- colorBin("YlOrRd", domain = county_estimates$pop_per_atty_county, bins = bins)
+    
+    # Set what appears when you mouseover the counties map
+    counties_tooltip <- paste("<p><strong>", county_estimates$NAME, "</strong></p>",
+                      "<strong>","Served by:", "</strong>", county_estimates$office, "<br>",
+                      "<strong>","TRLA Attorneys:", "</strong>", county_estimates$attys, "<br>",
+                      "<strong>","Under 200%:", "</strong>", county_estimates$Under200, "±", round(county_estimates$Under200MOE,2), "<br>",
+                      "<strong>","Eligible clients per attorney:", "</strong>", county_estimates$pop_per_atty_county) %>%
+      lapply(htmltools::HTML)
+    
+    
+    # COUNTIES MAP
+    
+    county_map <- leaflet() %>%
+      addProviderTiles("CartoDB.Positron") %>%
+      addPolygons(data = county_estimates,
+                  fillColor = ~palette_counties(county_estimates$pop_per_atty_county),
+                  weight = .6,
+                  fillOpacity = .8,
+                  smoothFactor = .8,
+                  label = counties_tooltip) %>%
+      addPolygons(data = region_estimates,
+                  weight = 3,
+                  fill = FALSE)
+    
+    
+    # 2) Build region map
+    
+    palette_regions <- colorBin("YlOrRd", domain = region_estimates$pop_per_atty_region, bins = 5)
+    
+    # Set what appears when you mouseover the region map
+    region_tooltip <- paste("<p><strong>", region_estimates$office, "</strong></p>",
+                            "<strong>","Under 200%:", "</strong>", region_estimates$poverty_pop_region, "<br>",
+                            "<strong>","TRLA Attorneys:", "</strong>", region_estimates$attys, "<br>",
+                            "<strong>","Eligible clients per attorney:", "</strong>", region_estimates$pop_per_atty_region) %>%
+      lapply(htmltools::HTML)
+    
+    
+    # REGIONS/OFFICES MAP
+    
+    region_map <- leaflet() %>%
+      addProviderTiles("CartoDB.Positron") %>%
+      addPolygons(data = county_estimates,
+                  fill = FALSE,
+                  weight = 2) %>%
+      addPolygons(data = region_estimates,
+                  fillColor = ~palette_regions(region_estimates$pop_per_atty_region),
+                  weight = 2,
+                  fillOpacity = .8,
+                  smoothFactor = .8,
+                  label = region_tooltip)
+    
+    
+    maps <- list(county_map, region_map)
+    
+    return(maps)
+
+}
 
 
-bins <- c(0, 100, 200, 500, 1000, 10000, 15000, 20000, Inf)
 
-pal <- colorBin("YlOrRd", domain = TRLA_regions$ppl_per_atty_region, bins = 5)
-pal2 <- colorBin("YlOrRd", domain = TRLA_counties$ppl_per_atty_cty, bins = bins)
+# Produce maps with all civil attorneys
 
-content <- paste("<p><strong>", TRLA_regions$Office, "</strong></p>",
-                 "<strong>","Under 200%:", "</strong>", TRLA_regions$Poverty_pop_region, "<br>",
-                 "<strong>","TRLA Attorneys:", "</strong>", TRLA_regions$attys, "<br>",
-                 "<strong>","Eligible clients per attorney:", "</strong>", TRLA_regions$ppl_per_atty_region) %>%
-  lapply(htmltools::HTML)
+estimates <- get_estimates(poverty_data, attys_list, trla_counties)
 
-content2 <- paste("<p><strong>", TRLA_counties$NAME, "</strong></p>",
-                  "<strong>","Served by:", "</strong>", TRLA_counties$Office, "<br>",
-                  "<strong>","TRLA Attorneys:", "</strong>", TRLA_counties$attys, "<br>",
-                  "<strong>","Under 200%:", "</strong>", TRLA_counties$Under200, "±", round(TRLA_counties$Under200MOE,2), "<br>",
-                  "<strong>","Eligible clients per attorney:", "</strong>", TRLA_counties$ppl_per_atty_cty) %>%
-  lapply(htmltools::HTML)
+county_estimates <- estimates[[1]]
+region_estimates <- estimates[[2]]
+
+maps <- build_maps(county_estimates, region_estimates)
+
+county_map <- maps[[1]]
+region_map <- maps[[2]]
 
 
 
-#The .html outputs for these two leaflet maps were produced by 
-# selecting Export > Save as web page from the Viewer
+# Produce maps without law grads
+
+estimates_nlg <- get_estimates(poverty_data, attys_list_nlg, trla_counties)
+
+county_estimates_nlg <- estimates_nlg[[1]]
+region_estimates_nlg <- estimates_nlg[[2]]
+
+maps_nlg <- build_maps(county_estimates_nlg, region_estimates_nlg)
+
+county_map_nlg <- maps_nlg[[1]]
+region_map_nlg <- maps_nlg[[2]]
 
 
-#REGIONS/OFFICES
+saveWidget(county_map, file="./Output/2021_09_by_county.html")
+saveWidget(region_map, file="./Output/2021_09_by_region.html")
 
-leaflet() %>%
-  addProviderTiles("CartoDB.Positron") %>%
-  addPolygons(data = TRLA_counties,
-              fill = FALSE,
-              weight = 2) %>%
-  addPolygons(data = TRLA_regions,
-              fillColor = ~pal(TRLA_regions$ppl_per_atty_region),
-              weight = 2,
-              fillOpacity = .8,
-              smoothFactor = .8,
-              label = content)
+saveWidget(county_map_nlg, file="./Output/2021_09_by_county_no_law_grads.html")
+saveWidget(region_map_nlg, file="./Output/2021_09_by_region_no_law_grads.html")
 
-#COUNTIES
 
-leaflet() %>%
-  addProviderTiles("CartoDB.Positron") %>%
-  addPolygons(data = TRLA_counties,
-              fillColor = ~pal2(TRLA_counties$ppl_per_atty_cty),
-              weight = .6,
-              fillOpacity = .8,
-              smoothFactor = .8,
-              label = content2) %>%
-  addPolygons(data = TRLA_regions,
-              weight = 3,
-              fill = FALSE)
+# Next steps: would be great to have this as a single file with selectable layers & dynamic legend
+# Reproduce the data layers excluding law grads
+# Add two addPolygons to the map
+# Add layerscontrol to allow toggle (example below)
+# Make sure that the legend updates accordingly
+
+# #  addLayersControl(
+# baseGroups = c("OSM (default)", "Toner", "Toner Lite"),
+# overlayGroups = c("Quakes", "Outline"),
+# options = layersControlOptions(collapsed = FALSE)
+# )
 
--- Scripts/poverty-estimates-to-spreadsheet.R
+++ Scripts/poverty-estimates-to-spreadsheet.R
@@ -10,15 +10,11 @@
 # I know this is easy stuff for data people, but it is a place for me to start.
 
 
-# for all Texas counties, and for all TRLA service area counties:
-
-## Population, population under 200% of poverty
-## Population by race/ethnicity
-
-
-## Population in poverty by race /ethnicity
-## Changes in these numbers ... 
-
+# For all Texas counties, and for all TRLA service area counties:
+# A workbook with 3 tabs
+# Population under 200% of poverty level
+# Population by race/ethnicity
+# Population by under 200% of poverty level by race/ethnicity
 
 library(tidyverse)
 library(tidycensus)
@@ -37,6 +33,54 @@ key <- read.delim("../credentials/census_api_key.txt", header = FALSE)
 
 census_api_key(key)
 
+
+## ------ Adapting Lizzie's code from Poverty Estimates.R
+
+# Get population estimates under 200% of poverty level by county
+tx_poverty <- get_acs(geography = "county",
+                      table = "C17002",
+                      survey = "acs5",
+                      year = 2019,
+                      state = "TX",
+                      output = "tidy",
+                      geometry = FALSE
+)
+
+
+# All Texas counties
+pop_under_200_tx <- tx_poverty %>%
+  filter(variable == "C17002_002" | 
+         variable == "C17002_003" | 
+         variable == "C17002_004" | 
+         variable == "C17002_005" | 
+         variable == "C17002_006" | 
+         variable == "C17002_007") %>%
+  group_by(GEOID) %>%
+  mutate(Under200 = sum(estimate),
+         MOE = moe_sum(moe,estimate)) %>%
+  filter(variable == "C17002_002") %>%
+  select(-estimate, -moe)
+
+
+# Filter to TRLA service area
+pop_under_200_sa <- subset(pop_under_200_tx, GEOID %in% trla_counties$GEOID)
+
+
+
+
+
+
+# write.xlsx(as.data.frame(pop_under_200), "Under200_2019_5y.xlsx")
+
+
+
+
+
+
+
+
+#### ------Anna testing: 
+
 # Save library of variables
 v19 <- load_variables(2019, "acs5", cache = TRUE) 
 
@@ -44,32 +88,33 @@ v19 <- load_variables(2019, "acs5", cache = TRUE)
 # Obtain county -level demographic estimates with geometry from the 2019 ACS 5-Year Estimates
 # Reformat & add fields for percentages
 acs_19_tx <- get_acs(geography = "county",
-                  variables = c(
-                    total_pop = "C17002_001", 
-                    p2 = "C17002_002", # Pop under 50% of poverty level
-                    p3 = "C17002_003", # 50-99%
-                    p4 = "C17002_004", # 100 - 1.24%
-                    p5 = "C17002_005", # 1.25 - 1.5%
-                    p6 = "C17002_006",
-                    p7 = "C17002_007",
-                    total_pop2 = "B03002_001",
-                    median_income = "B25119_001",
-                    white = "B03002_003", # white non-Hispanic
-                    black = "B03002_004", # Black non-Hispanic
-                    asian = "B03002_006", # Asian non-Hispanic
-                    hispanic = "B03002_012", # Hispanic all races
-                    american_indian = "B03002_007"), # American Indian, non-Hispanic
-                  state = "TX",
-                  geometry = FALSE) %>%
+                     variables = c(
+                       total_pop_poverty = "C17002_001", 
+                       p2 = "C17002_002", # Pop under 50% of poverty level
+                       p3 = "C17002_003", # 50-99%
+                       p4 = "C17002_004", # 100 - 1.24%
+                       p5 = "C17002_005", # 1.25 - 1.5%
+                       p6 = "C17002_006",
+                       p7 = "C17002_007",
+                       total_pop_race = "B03002_001",
+                       white = "B03002_003", # white non-Hispanic
+                       black = "B03002_004", # Black non-Hispanic
+                       asian = "B03002_006", # Asian non-Hispanic
+                       hispanic = "B03002_012", # Hispanic all races
+                       american_indian = "B03002_007"), # American Indian, non-Hispanic
+                     state = "TX",
+                     geometry = FALSE) %>%
   dplyr::select("NAME", "GEOID", "variable", "estimate") %>%
   tidyr::spread(variable, estimate) %>%
   mutate(total_under_200_poverty = (p2 + p3 + p4 + p5 + p6 + p7),
-         pct_under_200_poverty = (total_under_200_poverty/total_pop)*100, 
-         pct_hispanic = (hispanic/total_pop)*100,
-         pct_white = (white/total_pop)*100,
-         pct_black = (black/total_pop)*100,
-         pct_asian = (asian/total_pop)*100,
-         pct_hispanic = (hispanic/total_pop)*100)
+         pct_under_200_poverty = (total_under_200_poverty/total_pop_poverty)*100, 
+         pct_hispanic = (hispanic/total_pop_race)*100,
+         pct_white = (white/total_pop_race)*100,
+         pct_black = (black/total_pop_race)*100,
+         pct_asian = (asian/total_pop_race)*100,
+         pct_hispanic = (hispanic/total_pop_race)*100)
+
+
 
 
 # Use county GEOID to subset the ACS data to counties in TRLA's service area
