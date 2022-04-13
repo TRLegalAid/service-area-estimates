b9b130720b84c6d2973b90b923ff5bc014b54408
amnovak <anovak@Annas-MacBook-Air.local>
2022-04-13 07:09:31 -0500

    Calculate estiamtes for 125% FPL

--- Scripts/2022-04-needs-assessment.R
+++ Scripts/2022-04-needs-assessment.R
@@ -57,7 +57,7 @@ acs_5yr_20 <- get_acs(geography = "county",
 )
 
 
-acs_5yr_20_trla <- subset(acs_5yr_20, GEOID %in% trla_counties$i_geoid)
+acs_5yr_20_trla <- subset(acs_5yr_20, GEOID %in% trla_counties$geoid)
 
 
 # Estimate for under 200% FPL
@@ -81,6 +81,8 @@ under_200_e_2020 <- sum(poverty_data_20$Under200) # 3060423
 under_200_moe_2020 <- sum(poverty_data_20$Under200MOE_sq) %>% sqrt() # 30005.46
 
 
+
+# Estimate - total population in TRLA service area, 2016-2020
 poverty_data_20_total <- acs_5yr_20_trla %>%
   filter(variable == "C17002_001") %>% mutate(moe_sq = moe * moe)
 
@@ -94,32 +96,22 @@ total_pop_moe_2020 <- sum(poverty_data_20_total$moe_sq) %>% sqrt() # 4045.143
 
 # Estimate for under 125% FPL
 
-poverty_data_20 <- acs_5yr_20_trla %>%
-  filter(variable == "C17002_002" | variable == "C17002_003" | variable == "C17002_004" | variable == "C17002_005"
-         | variable == "C17002_006" | variable == "C17002_007") %>%
+poverty_data_20_125 <- acs_5yr_20_trla %>%
+  filter(variable == "C17002_002" | variable == "C17002_003" | variable == "C17002_004") %>%
   group_by(GEOID) %>%
-  mutate(Under200 = sum(estimate),
-         Under200MOE = moe_sum(moe,estimate),
-         Under200MOE_sq = (Under200MOE * Under200MOE),
-         Perc_MOE = round(Under200MOE/Under200 * 100, 2)) %>%
+  mutate(Under125 = sum(estimate),
+         Under125MOE = moe_sum(moe,estimate),
+         Under125MOE_sq = (Under125MOE * Under125MOE),
+         Perc_MOE = round(Under125MOE/Under125 * 100, 2)) %>%
   filter(variable == "C17002_002") %>%
   select(-estimate, -moe) %>%
   mutate(NAME = str_replace(NAME, " County, Texas", ""))
 
 
-# Estimate - population under 200% FPL in TRLA service area, 2016-2020 
-under_200_e_2020 <- sum(poverty_data_20$Under200) # 3060423
+# Estimate - population under 125% FPL in TRLA service area, 2016-2020 
+under_125_e_2020 <- sum(poverty_data_20_125$Under125) # 1824714
 
-under_200_moe_2020 <- sum(poverty_data_20$Under200MOE_sq) %>% sqrt() # 30005.46
-
-
-poverty_data_20_total <- acs_5yr_20_trla %>%
-  filter(variable == "C17002_001") %>% mutate(moe_sq = moe * moe)
-
-total_pop_e_2020 <- sum(poverty_data_20_total$estimate) # 8252710
-total_pop_moe_2020 <- sum(poverty_data_20_total$moe_sq) %>% sqrt() # 4045.143
-
-
+under_125_moe_2020 <- sum(poverty_data_20_125$Under125MOE_sq) %>% sqrt() # 22841
 
 
 
@@ -141,7 +133,7 @@ acs_5yr_15 <- get_acs(geography = "county",
 
 
 
-acs_5yr_15_trla <- subset(acs_5yr_15, GEOID %in% trla_counties$i_geoid)
+acs_5yr_15_trla <- subset(acs_5yr_15, GEOID %in% trla_counties$geoid)
 
 
 poverty_data_15 <- acs_5yr_15_trla %>%
@@ -158,16 +150,40 @@ poverty_data_15 <- acs_5yr_15_trla %>%
 
 
 # Estimate - population under 200% FPL in TRLA service area, 2011-2015 
-under_200_e_2015 <- sum(poverty_data_15$Under200) # 
+under_200_e_2015 <- sum(poverty_data_15$Under200) # 3262988
 
-under_200_moe_2015 <- sum(poverty_data_15$Under200MOE_sq) %>% sqrt() #
+under_200_moe_2015 <- sum(poverty_data_15$Under200MOE_sq) %>% sqrt() #26247
 
 
+
+# Estimate - total population in TRLA service area, 2011-2015
 poverty_data_15_total <- acs_5yr_15_trla %>%
   filter(variable == "C17002_001") %>% mutate(moe_sq = moe * moe)
 
-total_pop_e_2015 <- sum(poverty_data_15_total$estimate) # 
-total_pop_moe_2015 <- sum(poverty_data_15_total$moe_sq) %>% sqrt() #
+total_pop_e_2015 <- sum(poverty_data_15_total$estimate) # 7653132
+total_pop_moe_2015 <- sum(poverty_data_15_total$moe_sq) %>% sqrt() # 4040.11
+
+
+
+# Estimate - population under 125% FPL in TRLA service area, 2011-2015 
+
+poverty_data_15_125 <- acs_5yr_15_trla %>%
+  filter(variable == "C17002_002" | variable == "C17002_003" | variable == "C17002_004") %>%
+  group_by(GEOID) %>%
+  mutate(Under125 = sum(estimate),
+         Under125MOE = moe_sum(moe,estimate),
+         Under125MOE_sq = (Under125MOE * Under125MOE),
+         Perc_MOE = round(Under125MOE/Under125 * 100, 2)) %>%
+  filter(variable == "C17002_002") %>%
+  select(-estimate, -moe) %>%
+  mutate(NAME = str_replace(NAME, " County, Texas", ""))
+
+
+# Estimate - population under 125% FPL in TRLA service area, 2011-2015 
+under_125_e_2015 <- sum(poverty_data_15_125$Under125) # 2017372
+
+under_125_moe_2015 <- sum(poverty_data_15_125$Under125MOE_sq) %>% sqrt() #20223
+
 
 
 
